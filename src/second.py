23d2bb4d1b9b502e1e216a728bef5bda53244e61
pmukhopadhyay <palash.mukhopadhyay@calsoftlabs.com>
2014-03-20 18:27:49 +0530

    cleanup

--- src/second.py
+++ src/second.py
@@ -3,26 +3,6 @@ from mako.lookup import TemplateLookup
 from mako.template import Template
 lookup = TemplateLookup(directories=['../html'])
 
-"""
-desc Device;
-+---------------+--------------+------+-----+---------+-------+
-| Field         | Type         | Null | Key | Default | Extra |
-+---------------+--------------+------+-----+---------+-------+
-| id            | varchar(255) | NO   | PRI | NULL    |       |
-| deviceId      | varchar(255) | NO   |     | NULL    |       |
-| deviceOS      | varchar(255) | NO   |     | NULL    |       |
-| certificateId | varchar(255) | YES  |     | NULL    |       |
-| user_id       | varchar(255) | YES  | MUL | NULL    |       |
-| deviceModel   | varchar(255) | YES  |     | NULL    |       |
-| deviceType    | varchar(255) | YES  |     | NULL    |       |
-| osVersion     | varchar(255) | YES  |     | NULL    |       |
-| isBlocked     | bit(1)       | NO   |     | b'0'    |       |
-| createdOn     | datetime     | YES  |     | NULL    |       |
-| lastUsedOn    | datetime     | YES  |     | NULL    |       |
-+---------------+--------------+------+-----+---------+-------+
-
-"""
-
 class Root:
     names = ["First","Second","Third"]
     def index(self):
